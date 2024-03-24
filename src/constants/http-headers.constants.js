55ef1db816775066af748b5ee33cd531e6e3a489
RillSvai <kirilkvas07@gmail.com>
2024-03-24 14:12:41 +0200

    feature: create http server and first test endpoints

---
+++ src/constants/http-headers.constants.js
@@ -0,0 +1,11 @@
+export const HTTP_STATUS_CODE = Object.freeze({
+  OK: 200,
+  NOT_FOUND: 404,
+});
+
+export const CONTENT_TYPE = Object.freeze({
+  PLAIN_TEXT: 'text/plain',
+  ICON: 'image/x-icon',
+});
+
+export const CONTENT_TYPE_KEY = 'Content-Type';
